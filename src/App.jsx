54829a63930f97908ae8a4ed0f1c1bf695fa0027
Diógenes Pasqualini <diogenes.pasqualini@softpark.com.br>
2026-05-25 14:12:26 -0300

    home-mobile-ok

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,7 @@ import Menino from "./pages/blog/Menino.jsx";
 import Sidebar from "./components/Sidebar";
 import ScrollToTop from "./components/ScrollToTop";
 import MobileHeader from "./components/MobileHeader";
+import MobileFooter from "./components/MobileFooter";
 
 import "./App.css";
 
@@ -65,6 +66,7 @@ const App = () => {
 
       <ScrollToTop />
       <MobileHeader setMenuOpen={setMenuOpen} />
+      <MobileFooter />
 
       <button
         onClick={() => setMenuOpen(true)}
---
+++ src/components/MobileFooter.jsx
@@ -0,0 +1,56 @@
+import { Link } from "react-router-dom";
+
+import "../assets/styles/MobileFooter.css";
+
+const MobileFooter = () => {
+
+  return (
+
+    <footer className="mobile-footer">
+
+      <Link to="/amigos" className="mobile-footer-item">
+
+        <img
+          src="/img/icons/amigos.svg"
+          alt="Amigos"
+        />
+
+      </Link>
+
+      <Link to="/lugares" className="mobile-footer-item">
+
+        <img
+          src="/img/icons/lugares.svg"
+          alt="Lugares"
+        />
+
+      </Link>
+
+      <a
+        href="https://wa.me/"
+        className="mobile-footer-item"
+      >
+
+        <img
+          src="/img/icons/whats.svg"
+          alt="WhatsApp"
+        />
+
+      </a>
+
+      <Link to="/blog" className="mobile-footer-item">
+
+        <img
+          src="/img/icons/blog.svg"
+          alt="Blog"
+        />
+
+      </Link>
+
+    </footer>
+
+  );
+
+};
+
+export default MobileFooter;
