6a2030ebee3457a3010961a3d237b55d1c8d8006
Diógenes Pasqualini <diogenes.pasqualini@softpark.com.br>
2026-06-10 15:39:11 -0300

    Admin blog finalizado com autenticacao Supabase

--- src/admin/components/AdminHeader.jsx
+++ src/admin/components/AdminHeader.jsx
@@ -1,4 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../../services/supabase";
+
 export default function AdminHeader() {
+    const navigate = useNavigate();
+
+  async function sair() {
+
+    await supabase.auth.signOut();
+
+    navigate("/admin");
+
+  }
 
   return (
 
@@ -23,6 +35,13 @@ export default function AdminHeader() {
           src="/img/blog/didifotocircular.webp"
           alt="Administrador"
           className="admin-avatar"
+        />
+        <img
+          src="/img/icons/deslogar.svg"
+          alt="Sair"
+          title="Sair"
+          className="logout-icon"
+          onClick={sair}
         />
 
       </div>
