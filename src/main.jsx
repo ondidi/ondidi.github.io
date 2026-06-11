0e64d104bd5feede940996477d7bcc17789b8120
Diógenes Pasqualini <diogenes.pasqualini@softpark.com.br>
2026-06-11 18:24:55 -0300

    fix github pages routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,25 @@ import App from "./App";
 
 import "./assets/styles/Index.css";
 
+/* GITHUB PAGES REDIRECT */
+
+const redirect =
+  sessionStorage.redirect;
+
+if (redirect) {
+
+  delete sessionStorage.redirect;
+
+  window.history.replaceState(
+    null,
+    null,
+    redirect
+  );
+
+}
+
+/* ===================== */
+
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
