de451b6e7cd609c888f296d40af4e1ec290e077a
Diógenes Pasqualini <diogenes.pasqualini@softpark.com.br>
2026-08-01 10:12:35 -0300

    espaço-paragrafo

--- src/components/blog/BlogArticleContent.tsx
+++ src/components/blog/BlogArticleContent.tsx
@@ -12,23 +12,25 @@ export default function BlogArticleContent({
 
 }: Props) {
 
+    console.log(JSON.stringify(artigo.texto_completo));
+
     return (
 
         <article className={styles.article}>
 
             <div
-
                 className={styles.content}
-
                 style={{
-
                     fontSize: `calc(1em + ${fontScale}rem)`
-
                 }}
-
             >
 
-                {artigo.texto_completo}
+                {artigo.texto_completo
+                    ?.split(/\r?\n/)
+                    .filter((paragrafo: string) => paragrafo.trim() !== "")
+                    .map((paragrafo: string, index: number) => (
+                        <p key={index}>{paragrafo}</p>
+                    ))}
 
             </div>
 
